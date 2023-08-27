168519aaac20632ee458261b575bbcd0be7b5779
AgustinDi <agustindigiacinto18@gmail.com>
2023-08-27 15:15:17 -0300

    transparency github redirect working

--- src/cards/GithubRedirect.jsx
+++ src/cards/GithubRedirect.jsx
@@ -3,7 +3,7 @@ import './Cards Styles/GithubRedirect.css'
 export default function GithubRedirect() {
      return (
           <div className='GithubRedirect clickeable'>
-               GithubRedirect
-          </div>
+               github redirect
+          </div >
      )
 }
