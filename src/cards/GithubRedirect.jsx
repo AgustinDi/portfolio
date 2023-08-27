ef59898a3c10a4a8ced5b6362ba967963ea9ff85
AgustinDi <agustindigiacinto18@gmail.com>
2023-08-27 15:17:43 -0300

    github redirect working

--- src/cards/GithubRedirect.jsx
+++ src/cards/GithubRedirect.jsx
@@ -2,8 +2,8 @@ import './Cards Styles/GithubRedirect.css'
 
 export default function GithubRedirect() {
      return (
-          <div className='GithubRedirect clickeable'>
+          <a href='https://github.com/AgustinDi' target='_Blank' className='GithubRedirect clickeable' rel="noreferrer">
                github redirect
-          </div >
+          </a >
      )
 }
