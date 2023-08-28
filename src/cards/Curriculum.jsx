34c9eb5a2b4d68dc7a5dab409db18378e995f4c1
AgustinDi <agustindigiacinto18@gmail.com>
2023-08-28 19:21:01 -0300

    CV card - working

--- src/cards/Curriculum.jsx
+++ src/cards/Curriculum.jsx
@@ -2,8 +2,12 @@ import './Cards Styles/Curriculum.css'
 
 export default function Curriculum() {
      return (
-          <div className='Curriculum clickeable'>
-               Curriculum
-          </div>
+          <a href='https://www.linkedin.com/in/agustin-digiacinto/overlay/1635502083443/single-media-viewer/?profileId=ACoAADbea4gBRvhAlPrYNncXsqiaLFPTOHRhEm8' target='_Blank' className='Curriculum clickeable'>
+               <marquee className='Curriculum_marquee_father' behavior='alternate' scrollamount='10' direction='down'>
+                    <marquee className='Curriculum_marquee_child' behavior='alternate' scrollamount='10'>
+                         <h1 className='Curriculum_bounceText'>CV</h1>
+                    </marquee>
+               </marquee>
+          </a>
      )
 }
