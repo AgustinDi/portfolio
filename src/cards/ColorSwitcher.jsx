a8b1a27f62688a781ae7034c1cad9cf009955a7e
AgustinDi <agustindigiacinto18@gmail.com>
2023-08-29 20:18:55 -0300

    ColorSwitcher css working

--- src/cards/ColorSwitcher.jsx
+++ src/cards/ColorSwitcher.jsx
@@ -3,7 +3,10 @@ import './Cards Styles/ColorSwitcher.css'
 export default function ColorSwitcher() {
      return (
           <div className="ColorSwitcher">
-               <button>switcher</button>
+               <label className="ColorSwitcher_switch">
+                    <input type="checkbox" />
+                    <span className="ColorSwitcher_slider"></span>
+               </label>
           </div>
      )
 }
