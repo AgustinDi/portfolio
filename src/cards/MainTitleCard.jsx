1d84e3c4d30216720a6b84c27559fce5bfa40f72
AgustinDi <agustindigiacinto18@gmail.com>
2023-08-29 19:20:09 -0300

    MainTitleCard - initial css

--- src/cards/MainTitleCard.jsx
+++ src/cards/MainTitleCard.jsx
@@ -3,7 +3,11 @@ import './Cards Styles/MainTitleCard.css'
 export default function MainTitle() {
      return (
           <div className='MainTitle'>
-               MainTitle
+               <img src='/me.png' className='MainTitle_Image' />
+               <div className='MainTitle_InfoContainer'>
+                    <h1>Agustin Di giacinto</h1>
+                    <h3>Fullstack Developer</h3>
+               </div>
           </div>
      )
 }
