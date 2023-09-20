87cd7e74a4b2143dc51ae7ecbd6dcedcfa682a17
AgustinDi <agustindigiacinto18@gmail.com>
2023-09-20 10:32:50 -0300

    sobre mi texto

--- src/cards/AboutMe.jsx
+++ src/cards/AboutMe.jsx
@@ -3,7 +3,12 @@ import './Cards Styles/AboutMe.css'
 export default function AboutMe() {
      return (
           <div className='AboutMe'>
-               AboutMe
+               <h3>Sobre Mi:</h3>
+               <p>
+                    Joven Trabajador en busqueda de Profesionalizarse, con excelencia
+                    en resolución de problemas y aprendizaje rapido de nuevos temas,
+                    comunicación y mucho entusiasmo al trabajar en equipo.
+               </p>
           </div>
      )
 }
